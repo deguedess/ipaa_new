1731105ac122e31893b533982149348dd85dcd4b
iuri <iuri.guedes.emer@gmail.com>
2022-02-04 08:01:35 -0300

    Signed-off-by: iuri <iuri.guedes.emer@gmail.com>

---
+++ Polls/forms.py
@@ -0,0 +1,129 @@
+import datetime
+
+from dataclasses import field
+from django.core.exceptions import NON_FIELD_ERRORS
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from Polls.models import Acao, Pergunta, Resposta, Respostas_usuario, Usuario
+from Portfolio.models import Carteiras
+#from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+class RegisterUserForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = "__all__"
+        error_messages = {
+            NON_FIELD_ERRORS: {
+                'unique_together': "%(model_name)s's %(field_labels)s are not unique.",
+            }
+        }
+        widgets = {
+            'idade': forms.NumberInput(attrs={
+                'autofocus': True
+            })
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        self.fields['perfil'].required = False
+        self.fields['genero'].required = True
+
+    def clean(self):
+        cleaned_data = super().clean()
+        idade = cleaned_data.get('idade')
+
+        if (idade < 10 or idade > 99):
+            self.add_error('idade', 'Idade Inválida')
+
+
+class Perguntas(forms.ModelForm):
+    class Meta:
+        model = Pergunta
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(Perguntas, self).__init__(*args, **kwargs)
+
+
+class SurveyForm(forms.Form):
+
+    #question_1 = forms.ChoiceField(widget=forms.RadioSelect, choices=())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        #del self.fields["question_1"]
+        for pergunta in Pergunta.objects.order_by('sequencia'):
+            respostas = [(resposta.id, resposta.resposta)
+                         for resposta in pergunta.resposta_set.all()]
+            self.fields[f"pergunta_{pergunta.id}"] = forms.ChoiceField(
+                widget=forms.RadioSelect, choices=respostas)
+            self.fields[f"pergunta_{pergunta.id}"].label = pergunta.pergunta
+
+    def save(self, userid):
+        data = self.cleaned_data
+
+        for pergunta in Pergunta.objects.order_by('sequencia'):
+            choice = Resposta.objects.get(pk=data[f"pergunta_{pergunta.id}"])
+
+            resp_user = Respostas_usuario()
+            resp_user.data_inicial = datetime.datetime.now()
+            resp_user.usuario = Usuario.objects.get(pk=userid)
+            resp_user.resposta = choice
+            resp_user.save()
+
+
+class PortfolioForm(forms.Form):
+
+    def __init__(self, acoes, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        acoesAll = Acao.objects.order_by('codigo')
+
+        # adiciona todas as ações recomendadas antes
+        if (acoes != None):
+            for acao in acoes:
+                # remove essa ação da lista geral para nao duplicar
+                acoesAll = acoesAll.exclude(pk=acao.id)
+
+                criaCamposForm.criaCamposBool(self, acao, True)
+
+        # adiciona o restante
+        for acao in acoesAll:
+            criaCamposForm.criaCamposBool(self, acao, False)
+
+    def save(self):
+        data = self.cleaned_data
+
+        selected = []
+
+        # mudar para buscar cfe usuario
+        for acao in Acao.objects.order_by('codigo'):
+            checked = data[f"acao_{acao.id}"]
+            if (checked):
+                selected.append(acao)
+
+        return selected
+
+    def clean(self):
+        data = super().clean()
+
+        # mudar para buscar cfe usuario
+        for acao in Acao.objects.order_by('codigo'):
+            checked = data[f"acao_{acao.id}"]
+            if (checked):
+                return
+
+        self.add_error(f"acao_{acao.id}", 'Selecione ao menos uma Ação')
+
+
+class criaCamposForm():
+    def criaCamposBool(form, obj, init):
+        form.fields[f"acao_{obj.id}"] = forms.BooleanField(
+            required=False, initial=init)
+        form.fields[f"acao_{obj.id}"].label = obj.codigo + \
+            ' - ' + obj.nome
---
+++ Polls/portfolio.py
@@ -0,0 +1,178 @@
+import datetime
+from Polls.admin import Carteira
+from Polls.models import Perfil, Respostas_usuario, Simulacao_cenarios, Usuario, Motivo
+from Portfolio.models import Carteiras, Hist_alt_carteira, Acao
+
+# Metodo para buscar as respostas do usuario e definir o perfil
+
+
+class calculaPortfolio():
+
+    def verificaPerfil(userid):
+
+        pontos = 0
+        for resposta in Respostas_usuario.objects.filter(usuario_id=userid):
+            pontos += resposta.resposta.pontuacao
+
+        ptos = int(pontos)
+
+        try:
+            perf = Perfil.objects.get(
+                peso_inicial__lte=ptos, peso_final__gte=ptos)
+
+        except Exception as e:
+            perf = 'Perfil Não Encontrado'
+            print(e)
+
+        return perf
+
+    def getMotivos(nomeMotivo):
+
+        try:
+            return Motivo.objects.get(nome=nomeMotivo)
+        except Exception as e:
+            motivo = Motivo()
+            motivo.nome = nomeMotivo
+            motivo.save()
+            return motivo
+
+
+# metodo para criação da carteira inicial
+
+    def criaCarteira(userid, tipoGrupo, acoes):
+
+        cart = calculaPortfolio.verificaCarteira(userid)
+
+        if (cart != None):
+            return cart
+
+        cart = Carteiras()
+        cart.usuario = Usuario.objects.get(pk=userid)
+        cart.tipo_grupo = tipoGrupo
+        cart.nome = 'Carteira ' + 'Automática' if tipoGrupo == 0 else 'Manual'
+        cart.save()
+        if (acoes != None):
+            cart.acoes.set(acoes)
+            cart.save()
+
+            if (cart.tipo_grupo == 0):
+                calculaPortfolio.registraAlteracoes(
+                    acoes, cart, 'C', True, True, None, None)
+
+        return cart
+
+    def verificaCarteira(userid):
+        cart = Carteiras.objects.filter(usuario_id=userid)
+
+        if (cart.count() == 0):
+            return None
+        else:
+            return cart[0]
+
+    def salvaHistoricoCarteiraManual(cart, acoesSel):
+
+        acoesCart = cart.acoes.all()
+
+        # verificar as acoes selecionadas
+        for acSel in acoesSel:
+            # Se a ação selecionada nao estiver na carteira, registra a alteração
+            if (acSel not in acoesCart):
+                calculaPortfolio.registraAlteracao(
+                    acSel, cart, 'C', False, False, None, None)
+
+        # verifica as ações na carteira
+        for acCat in acoesCart:
+            if (acCat not in acoesSel):
+                calculaPortfolio.registraAlteracao(
+                    acCat, cart, 'V', False, False, None, None)
+
+    def salvaHistoricoCarteiraIA(cart, acoesSel, acoesRec):
+        acoesCart = cart.acoes.all()
+
+        # verifica as acoes selecionadas = COMPRAS
+        for acSel in acoesSel:
+            # se a ação nao estiver na carteira, houve alteração, nesse caso ADICIONADA
+            if (acSel not in acoesCart):
+                # verifica se a alteração foi recomendada ou nao
+                if (acSel in acoesRec):
+                    calculaPortfolio.registraAlteracao(
+                        acSel, cart, 'C', True, True, None, None)
+                else:  # se nao for recomendação
+                    calculaPortfolio.registraAlteracao(
+                        acSel, cart, 'C', False, False, None, None)
+
+        # verificar as ações na carteira = VENDAS
+        for acCart in acoesCart:
+            # se a ação nao estiver na selecionadas, houve alteração, nesse caso foi REMOVIDA
+            if (acCart not in acoesSel):
+                # verificar se a venda foi recomendada ou nao
+                if (acCart in acoesRec):
+                    calculaPortfolio.registraAlteracao(
+                        acCart, cart, 'V', False, False, None, None)
+                else:  # se nao for recomendação
+                    calculaPortfolio.registraAlteracao(
+                        acCart, cart, 'V', True, True, None, None)
+
+         # Verifica as RECOMENDADAS
+        for acRec in acoesRec:
+            if (acRec not in acoesSel):
+                calculaPortfolio.registraAlteracao(
+                    acRec, cart, 'C', True, False, None, None)
+
+    def salvaPortfolio(carteira, acoesSelected, recomended):
+
+        print("1")
+        print(carteira.acoes.all())
+        print("2")
+        print(acoesSelected)
+        print("3")
+        print(recomended)
+
+        if (carteira.tipo_grupo == 0):
+            calculaPortfolio.salvaHistoricoCarteiraIA(
+                carteira, acoesSelected, recomended)
+        else:
+            calculaPortfolio.salvaHistoricoCarteiraManual(
+                carteira, acoesSelected)
+
+        # salva as acoes na carteira
+        carteira.acoes.set(acoesSelected)
+        carteira.save()
+        print('ta salvo')
+
+
+# metodo para salvar alteração de carteira
+
+
+    def registraAlteracao(acao, carteira, oper, recIA, segRec, motivo, simulacao):
+
+        if (motivo == None):
+            if (carteira.tipo_grupo == 0):
+                motivo = calculaPortfolio.getMotivos('Recomendação')
+            else:
+                motivo = calculaPortfolio.getMotivos('Escolha Pessoal')
+
+        hist = Hist_alt_carteira()
+        hist.acao = acao
+        hist.carteira = carteira
+        hist.data_alt = datetime.datetime.now()
+        hist.operacao = oper
+        hist.recomendacao_ia = recIA
+        hist.seguiu_recomendacao = segRec
+        hist.motivo = motivo
+        hist.simulacao = Simulacao_cenarios.objects.get(
+            pk=1) if simulacao == None else simulacao
+        hist.save()
+
+    def registraAlteracoes(acoes, carteira, oper, recIA, segRec, motivo, simulacao):
+        if (acoes.count() == 0):
+            return
+        for acao in acoes:
+            calculaPortfolio.registraAlteracao(
+                acao, carteira, oper, recIA, segRec, motivo, simulacao)
+
+
+# metodo para verificar quais as ações se encaixam no perfil
+
+    def calculaAcoes():
+        pass
