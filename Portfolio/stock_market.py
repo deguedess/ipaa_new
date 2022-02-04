585f74fe74141be4c25f3784bb20b853d5feb06d
iuri <iuri.guedes.emer@gmail.com>
2022-02-04 08:04:57 -0300

    Signed-off-by: iuri <iuri.guedes.emer@gmail.com>

--- Portfolio/stock_market.py
+++
@@ -1,7 +0,0 @@
-# importar as bibliotecas necessárias
-import pandas as pd
-from pandas_datareader import data as web
-import plotly.graph_objects as go
-
-# criar um DataFrame vazio
-df = pd.DataFrame()
