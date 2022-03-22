39b8f9206d0178e10c343d49559aceb9587f5257
iuri <iuri.guedes.emer@gmail.com>
2022-03-22 08:30:43 -0300

    Signed-off-by: iuri <iuri.guedes.emer@gmail.com>

--- IPAA_APP/Portfolio/stockClustering.py
+++ IPAA_APP/Portfolio/stockClustering.py
@@ -6,7 +6,12 @@ from Polls.simulation import calculaSimulacoes
 from Portfolio.stockPrediction import PrevisaoAcoes
 import numpy as np
 import pandas as pd
+from pandas_datareader import data
 from sklearn import cluster, covariance
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 
 class CategorizacaoAcoes():
@@ -14,47 +19,76 @@ class CategorizacaoAcoes():
     logCl = []
 
     def testeInfo():
+
+        #qtdeAtributos = 6
+        qtdePerfis = CategorizacaoAcoes.getQtdeNeceClusters()
+
         # Busca a primeira simulação, para pegar as datas
         simula = calculaSimulacoes.getSimulacaoInicial()
 
         # Pega todas as ações
         Acoes = Acao.objects.all()
 
-        codigos = []
-        dfs = []
-        for acao in Acoes:
-            cod = acao.codigo
+        acoesDic = CategorizacaoAcoes.converteAcoesToDict(Acoes)
 
-            # Busca as informações sobre as ações no periodo
-            df = PrevisaoAcoes.getValoresBolsa(
-                cod, simula.data_ini, simula.data_fim)
-            if (not df.empty):
+        # Carrega as informações das ações
+        df = data.DataReader(list(acoesDic.values()), data_source='yahoo',
+                             start=simula.data_ini, end=simula.data_fim)
 
-                codigos.append(cod)
-                sec_data = df['Adj Close']
-                # retorno diario
-                sec_returns = np.log(sec_data / sec_data.shift(1))
+        # Verifica o tamanho inicial do DF
+        #inicial = len(df.columns) / qtdeAtributos
 
-                df['Media'] = sec_returns.mean() * 250
-                df['Desvio'] = sec_returns.std() * 250 ** 0.5
+        # caso haja ações com problema no cadastro e os valores vierem NULL, então serão descartados
+        df = df.dropna(how='all', axis=1)
 
-                print('====> ' + cod)
+        # salva a quantidade total de colunas para saber quantas ações estavam com problema
+        #final = len(df.columns) / qtdeAtributos
 
-                dfs.append(df)
+        lenAcoes = len(acoesDic)
 
-        names = np.array(sorted(codigos)).T
+        # stock_open is numpy array of transpose of df['Open']
+        stock_open = np.array(df['Open']).T
+        # stock_close is numpy array of transpose of df['Close']
+        stock_close = np.array(df['Close']).T
 
-        std_prices = np.vstack([q["Desvio"] for q in dfs])
-        mean_prices = np.vstack([q["Media"] for q in dfs])
+        movements = stock_close - stock_open
 
-        std_prices = std_prices[~np.isnan(std_prices)]
+        # ‘sum_of_movement’ of a company is defined as sum of differences of closing and opening prices of all days.
+        sum_of_movement = np.sum(movements, 1)
 
-        print(std_prices)
+        # The company and its ‘sum_of_movement’ is printed.
+        # Have positive ‘sum_of_movement’. Hence it is advisable to go long(buy) on these stocks.
+        # Have negative ‘sum_of_movement’. Hence it is advisable to short(sell) the stocks.
+        for i in range(lenAcoes):
+            print('company:{}, Change:{}'.format(
+                df['High'].columns[i], sum_of_movement[i]))
 
-        # Gera os primeiros clusters
-        clusters = CategorizacaoAcoes.clusterizacao(
-            std_prices, names)
-        print(clusters)
+        # Define a normalizer
+        normalizer = Normalizer()
+        # Create Kmeans model
+        kmeans = KMeans(n_clusters=qtdePerfis, max_iter=1000)
+        # Make a pipeline chaining normalizer and kmeans
+        pipeline = make_pipeline(normalizer, kmeans)
+        # Fit pipeline to daily stock movements
+        pipeline.fit(movements)
+        labels = pipeline.predict(movements)
+
+        df1 = pd.DataFrame({'labels': labels, 'companies': list(
+            acoesDic)}).sort_values(by=['labels'], axis=0)
+        print(df1)
+
+    def converteAcoesToDict(acoes):
+        dicti = dict()
+        for acao in acoes:
+
+            try:
+                data.DataReader(acao.codigo + '.SA', data_source='yahoo',
+                                start='01-01-2020', end='05-01-2020')
+                dicti[acao.nome] = acao.codigo + '.SA'
+            except:
+                continue
+
+        return dicti
 
     def primeiraCategorizacao():
         CategorizacaoAcoes.logCl = []
@@ -81,10 +115,10 @@ class CategorizacaoAcoes():
         close_prices = np.vstack([q["Close"] for q in dfs])
         open_prices = np.vstack([q["Open"] for q in dfs])
 
+        print(close_prices)
+
         # Busca os dados da variaçao de preços (fechamento - abertura) para gerar a variação
         variation = close_prices - open_prices
-
-        print(variation)
 
         # Gera os primeiros clusters
         clusters = CategorizacaoAcoes.clusterizacao(variation, names)
