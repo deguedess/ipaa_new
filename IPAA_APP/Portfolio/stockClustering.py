9c3e965dec84d2f4ed8aef5b24b26de2af84baf9
iuri <iuri.guedes.emer@gmail.com>
2022-03-22 12:02:08 -0300

    Signed-off-by: iuri <iuri.guedes.emer@gmail.com>

--- IPAA_APP/Portfolio/stockClustering.py
+++ IPAA_APP/Portfolio/stockClustering.py
@@ -20,68 +20,67 @@ class CategorizacaoAcoes():
 
     def testeInfo():
 
-        #qtdeAtributos = 6
-        qtdePerfis = CategorizacaoAcoes.getQtdeNeceClusters()
-
         # Busca a primeira simulação, para pegar as datas
         simula = calculaSimulacoes.getSimulacaoInicial()
 
         # Pega todas as ações
         Acoes = Acao.objects.all()
 
+        # converte as ações em dicionários para geração da clusterização
         acoesDic = CategorizacaoAcoes.converteAcoesToDict(Acoes)
 
         # Carrega as informações das ações
         df = data.DataReader(list(acoesDic.values()), data_source='yahoo',
                              start=simula.data_ini, end=simula.data_fim)
 
-        # Verifica o tamanho inicial do DF
-        #inicial = len(df.columns) / qtdeAtributos
+        lenAcoes = len(acoesDic)  # Qtde de ações com dados
 
-        # caso haja ações com problema no cadastro e os valores vierem NULL, então serão descartados
-        df = df.dropna(how='all', axis=1)
-
-        # salva a quantidade total de colunas para saber quantas ações estavam com problema
-        #final = len(df.columns) / qtdeAtributos
-
-        lenAcoes = len(acoesDic)
-
-        # stock_open is numpy array of transpose of df['Open']
+        # Busca as informações dos preços de abertura
         stock_open = np.array(df['Open']).T
-        # stock_close is numpy array of transpose of df['Close']
+        # Busca as informações dos preços de fechamento
         stock_close = np.array(df['Close']).T
 
         movements = stock_close - stock_open
 
-        # ‘sum_of_movement’ of a company is defined as sum of differences of closing and opening prices of all days.
+        # a Soma de movimentação de uma companhia é definido somando as diferenças de fechamento e abertura durante os dias
         sum_of_movement = np.sum(movements, 1)
 
-        # The company and its ‘sum_of_movement’ is printed.
-        # Have positive ‘sum_of_movement’. Hence it is advisable to go long(buy) on these stocks.
-        # Have negative ‘sum_of_movement’. Hence it is advisable to short(sell) the stocks.
+        # Tendo uma soma positiva, significa COMPRA
+        # Tendo uma soma negativa, significa VENDA
         for i in range(lenAcoes):
             print('company:{}, Change:{}'.format(
                 df['High'].columns[i], sum_of_movement[i]))
 
+        dfClu = CategorizacaoAcoes.iniciaClusterizacao(movements, acoesDic)
+
+        for i in range(len(dfClu.index)):
+
+            CategorizacaoAcoes.salvaClusterAcao(
+                acao=dfClu['companies'][i], cluster=dfClu['labels'][i])
+
+    def iniciaClusterizacao(values, acoesDic):
         # Define a normalizer
         normalizer = Normalizer()
+        # Reduce the data
+        reduced_data = PCA(n_components=2)
         # Create Kmeans model
-        kmeans = KMeans(n_clusters=qtdePerfis, max_iter=1000)
+        kmeans = KMeans(
+            n_clusters=CategorizacaoAcoes.getQtdeClusters(), max_iter=1000)
         # Make a pipeline chaining normalizer and kmeans
-        pipeline = make_pipeline(normalizer, kmeans)
+        pipeline = make_pipeline(normalizer, reduced_data, kmeans)
         # Fit pipeline to daily stock movements
-        pipeline.fit(movements)
-        labels = pipeline.predict(movements)
+        pipeline.fit(values)
+        labels = pipeline.predict(values)
 
-        df1 = pd.DataFrame({'labels': labels, 'companies': list(
-            acoesDic)}).sort_values(by=['labels'], axis=0)
-        print(df1)
+        return pd.DataFrame({'labels': labels, 'companies': list(
+            acoesDic.values())}).sort_values(by=['labels'], axis=0)
 
     def converteAcoesToDict(acoes):
         dicti = dict()
         for acao in acoes:
 
             try:
+                # é usado apenas para verificar se a ação é valida
                 data.DataReader(acao.codigo + '.SA', data_source='yahoo',
                                 start='01-01-2020', end='05-01-2020')
                 dicti[acao.nome] = acao.codigo + '.SA'
@@ -90,78 +89,11 @@ class CategorizacaoAcoes():
 
         return dicti
 
-    def primeiraCategorizacao():
-        CategorizacaoAcoes.logCl = []
-
-        # Busca a primeira simulação, para pegar as datas
-        simula = calculaSimulacoes.getSimulacaoInicial()
-
-        # Pega todas as ações
-        Acoes = Acao.objects.all()
-
-        dfs = []
-        codigos = []
-        for acao in Acoes:
-
-            # Busca as informações sobre as ações no periodo
-            df = PrevisaoAcoes.getValoresBolsa(
-                acao.codigo, simula.data_ini, simula.data_fim)
-            if (not df.empty):
-                codigos.append(acao.codigo)
-                dfs.append(df)
-
-        names = np.array(sorted(codigos)).T
-
-        close_prices = np.vstack([q["Close"] for q in dfs])
-        open_prices = np.vstack([q["Open"] for q in dfs])
-
-        print(close_prices)
-
-        # Busca os dados da variaçao de preços (fechamento - abertura) para gerar a variação
-        variation = close_prices - open_prices
-
-        # Gera os primeiros clusters
-        clusters = CategorizacaoAcoes.clusterizacao(variation, names)
-
-        # Inicia o processo de configuração e salvamento das informações no banco
-        CategorizacaoAcoes.iniciaPosClusterizacao(clusters)
-
-    def iniciaPosClusterizacao(clusters):
-        # Verifica quantos perfis existem para fazer o reagrupamento
-        qtdePerfis = CategorizacaoAcoes.getQtdeNeceClusters()
-
-        # se a quantidade de perfis for igual ao dos clusters, então não precisa fazer nada
-        if (qtdePerfis == len(clusters)):
-
-            for cl in clusters:
-                tam = cl.size
-                for i in range(tam):
-                    CategorizacaoAcoes.salvaClusterAcao(
-                        cl[i], 'Cluster ' + str(i))
-            return
-
-        if (qtdePerfis > len(clusters)):
-            # se tiver mais perfis
-            dif = qtdePerfis - len(clusters)
-
-            for cl in clusters:
-                tam = cl.size
-                for i in range(tam):
-                    CategorizacaoAcoes.salvaClusterAcao(
-                        cl[i], 'Cluster ' + str(i))
-
-        else:
-            # se tiver mais clusters
-            dif = len(clusters) - qtdePerfis
-
-            for cl in clusters:
-                tam = cl.size
-                for i in range(tam):
-                    CategorizacaoAcoes.salvaClusterAcao(
-                        cl[i], 'Cluster ' + str(i))
-
     # Metodo que salva qual cluster a Ação faz parte
+
     def salvaClusterAcao(acao, cluster):
+        acao = acao.replace('.SA', '')
+
         ac = Acao.objects.get(codigo=acao)
 
         if (ac == None):
@@ -171,32 +103,7 @@ class CategorizacaoAcoes():
         ac.classificacao_ia = cluster
         ac.save()
         CategorizacaoAcoes.logCl.append(
-            'Acao: ' + acao + ' - ' + cluster)
+            'Acao: ' + acao + ' - ' + str(cluster))
 
-    def getQtdeNeceClusters():
+    def getQtdeClusters():
         return Perfil.objects.all().count()
-
-    # Metodo que faz o processo de Clusterização
-    def clusterizacao(value, acoesNomes):
-        # #############################################################################
-        # Learn a graphical structure from the correlations
-        edge_model = covariance.GraphicalLassoCV()
-
-        # standardize the time series: using correlations rather than covariance
-        # is more efficient for structure recovery
-        X = value.copy().T
-        X /= X.std(axis=0)
-        edge_model.fit(X)
-
-        # #############################################################################
-        # Cluster using affinity propagation
-        _, labels = cluster.affinity_propagation(
-            edge_model.covariance_, random_state=0)
-        n_labels = labels.max()
-
-        clusters = []
-        for i in range(n_labels + 1):
-            print(acoesNomes[labels == i])
-            clusters.append(acoesNomes[labels == i])
-
-        return clusters
